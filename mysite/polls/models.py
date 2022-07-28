2fdcadc1dd17cd0a717c08c14ab104ca92565fb0
Mustapha Adams <adamsmustapha419@gmail.com>
2022-07-28 22:50:10 +0000

    Generic views
    refactoring code

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -19,8 +19,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question,
-on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=255)
     votes = models.IntegerField(default=0) 
 
